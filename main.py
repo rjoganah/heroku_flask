296b39a3c67972d15bcd851a1c56f8ac31cea82c
robin joganah <robin.joganah.1@ulaval.ca>
2016-10-26 14:29:28 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+Created on 26 oct. 2016
+
+@author: jogr0001
+'''
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
